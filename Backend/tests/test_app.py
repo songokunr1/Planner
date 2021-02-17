e2686bd7394632b0cf73ee9e70bb64cd1f832f1c
songokunr1 <pawelm18@gmail.com>
2021-02-17 21:29:05 +0100

    prepared test_client in pytest for future testing

--- Backend/tests/test_app.py
+++ Backend/tests/test_app.py
@@ -3,12 +3,21 @@ import json
 import requests
 
 from Backend.app import app
+from Backend import create_test_app
+import pytest
 
 
 # @pytest.fixture
 # def app():
-#     app = app_b
-#     return app
+#     app = create_test_app()
+#     yield app
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
 
 def test_empty_db():
     """Start with a blank database."""
@@ -36,3 +45,8 @@ def test_post():
     r = requests.post(
         'http://127.0.0.1:8030/message', headers=headers, data=json.dumps(json_data)
     )
+
+class TestSomethings:
+    def test_post_2(self, client):
+        r = client.get("/")
+        assert r.status_code == 201
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from Backend import create_test_app, create_production_app, app
 
 if __name__ == '__main__':
-    app.run(port=8040, debug=True)
+    app.run(port=8030, debug=True)
