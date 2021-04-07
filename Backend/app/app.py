e5bb3654338610a7a4d9b87b2522e7087ec888b4
songokunr1 <pawelm18@gmail.com>
2021-04-07 20:39:49 +0200

    working with queue

--- Backend/app/app.py
+++ Backend/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 import requests
 # from . import create_test_app
 import json
@@ -7,9 +7,20 @@ from rq import Queue
 import time
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 db = SQLAlchemy()
 r = redis.Redis(host='rq-server', port=6379, decode_responses=True)
 q = Queue(connection=r)
+
+def example(seconds):
+    print('Starting task')
+    for i in range(seconds):
+        print(i)
+        time.sleep(1)
+    print('Task completed')
+
+
 
 
 def create_test_app():
@@ -30,7 +41,7 @@ def background_task(n):
 
     """ Function that returns len(n) and simulates a delay """
 
-    delay = 2
+    delay = 1
 
     print("Task running")
     print(f"Simulating a {delay} second delay")
@@ -45,18 +56,45 @@ def background_task(n):
 
 @app.route("/task")
 def task():
-
-    if request.args.get("n"):
+    task = request.args.get("n")
+    print(task)
+    if task:
         print("hejo")
-        job = q.enqueue(background_task, request.args.get("n"))
-
+        job = q.enqueue('app.tasks.background_task', request.args.get("n"))
+        print(job.get_id())
         return f"Task ({job.id}) added to queue at {job.enqueued_at}"
 
     return "No value for count provided"
 
+@app.route('/status/<job_id>')
+def job_status(job_id):
+    print(job_id)
+    job = q.fetch_job(r"f66b4ef3-446c-47a8-9156-ee82d6ff5442")
+    if job is None:
+        response = {'status': 'unknown'}
+    else:
+        response = {
+            'status': job.get_status(),
+            'result': job.result,
+        }
+        if job.is_failed:
+            response['message'] = job.exc_info.strip().split('\n')[-1]
+    return jsonify(response)
+
+
+@app.route("/create_task", methods=['GET', 'POST'])
+def create_task():
+    # task = request.form.get('task')
+    job = q.enqueue('app.tasks.background_task', 5)
+    print(job.get_id())
+    return jsonify({}), 202, {'Location': url_for('job_status', job_id=job.get_id())}
+
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    job = q.enqueue('app.tasks.example', 23)
+    print(job.get_id())
     return render_template("base.html")
 
 
@@ -65,6 +103,7 @@ def message():
     req = request.json
     print(req)
     return render_template("base.html")
+
 
 @app.route("/ba", methods=['GET'])
 def show_data():
